89253c65e04f2ff3eeac25adc4a90d225ab3f4ee
Chaing <35192749+nomysky52@users.noreply.github.com>
2019-05-23 20:34:21 +0800

    1.1.1.8

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const bot = linebot({
 // 當有人傳送訊息給Bot時 觸發
 bot.on('message', function(event) {
     var messagepush = 'userId:' + event.source.userId + '\n';
-	var displayName = event.source.profile().then(function (profile) {
-		return JSON.stringify(profile.displayName);
-    });
+	var displayName = JSON.stringify(event.source.profile().then(function (profile) {
+		return;
+    }));
 	messagepush = messagepush + 'displayName:' + displayName + '\n';
 	
     if(typeof event.source.groupId !== "undefined")
